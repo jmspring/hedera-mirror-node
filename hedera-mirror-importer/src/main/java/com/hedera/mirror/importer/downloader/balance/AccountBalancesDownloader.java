547edf77a368d5c1535f429b5308c82f84799d5e
Steven Sheehy <steven.sheehy@hedera.com>
2020-03-19 00:11:05 -0500

    Add HA to importer
    Signed-off-by: Steven Sheehy <steven.sheehy@hedera.com>

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/downloader/balance/AccountBalancesDownloader.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/downloader/balance/AccountBalancesDownloader.java
@@ -9,9 +9,9 @@ package com.hedera.mirror.importer.downloader.balance;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +22,6 @@ package com.hedera.mirror.importer.downloader.balance;
 
 import java.io.File;
 import javax.inject.Named;
-
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
@@ -30,6 +29,7 @@ import software.amazon.awssdk.services.s3.S3AsyncClient;
 import com.hedera.mirror.importer.addressbook.NetworkAddressBook;
 import com.hedera.mirror.importer.domain.ApplicationStatusCode;
 import com.hedera.mirror.importer.downloader.Downloader;
+import com.hedera.mirror.importer.leader.Leader;
 import com.hedera.mirror.importer.repository.ApplicationStatusRepository;
 
 @Log4j2
@@ -42,6 +42,7 @@ public class AccountBalancesDownloader extends Downloader {
         super(s3Client, applicationStatusRepository, networkAddressBook, downloaderProperties);
     }
 
+    @Leader
     @Override
     @Scheduled(fixedRateString = "${hedera.mirror.downloader.balance.frequency:500}")
     public void download() {
